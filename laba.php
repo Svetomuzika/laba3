284a4e603459d1a21c5c632f5300deec11e82d81
Artem <sozonov_artem@mail.ru>
2020-12-23 03:08:21 +0500

    Initial commit

--- laba.php
+++
@@ -1,7 +0,0 @@
-echo "# laba3" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Svetomuzika/laba3.git
-git push -u origin main
