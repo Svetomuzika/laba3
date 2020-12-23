db44f1436f80aa7211af7d595e6308230800411a
Artem <sozonov_artem@mail.ru>
2020-12-24 00:55:07 +0500

    update

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 require 'vendor/autoload.php';
 
-require 'src/magicclass.php';
-include 'src/magicclass.php';
+require_once "magicclass.php";
 
 $a = new MagicClass();
+function SayHi(){
+    return "Hi";
+}
+$a -> SayHi();
+
+
+
 
 
